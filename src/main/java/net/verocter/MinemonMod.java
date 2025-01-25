9a8244dfe728d80be398c005588e2eb8e4a57ee6
VictorMellado <victormm122003@gmail.com>
2025-01-25 22:12:24 +0100

    Initial Commit

---
+++ src/main/java/net/verocter/MinemonMod.java
@@ -0,0 +1,16 @@
+package net.verocter;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MinemonMod implements ModInitializer {
+	public static final String MOD_ID = "minemon-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/verocter/MinemonModClient.java
@@ -0,0 +1,10 @@
+package net.verocter;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinemonModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
